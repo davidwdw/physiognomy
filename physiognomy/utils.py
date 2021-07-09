04946ac51aa39ba6cfddba51e44d52ffadc5739b
davidwdw <davidwdw@gmail.com>
2021-07-09 14:29:51 -0500

    isnotebook

--- physiognomy/utils.py
+++ physiognomy/utils.py
@@ -22,6 +22,18 @@ from sklearn.model_selection import train_test_split
 
 
 
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True
+        elif shell == 'TerminalInteractiveShell':
+            return False
+        else:
+            return False
+    except NameError:
+        return False
+
 def Get_Flattened_Dict(d, parent_key='', sep='_'):
     '''Utility function that flattens nested dictionary
     from Face++ and returns a flattened dictionary
